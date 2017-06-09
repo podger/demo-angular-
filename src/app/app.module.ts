60e23843bb5aad3872c51b707600347626e10079
Roberto Limongello <roberto.limongello@reti.it>
2017-06-10 00:36:03 +0200

    Routing esercizio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { ClockComponent } from './components/clock/clock.component';
 import { NumericInputComponent } from './components/numeric-input/numeric-input.component';
@@ -10,13 +9,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 // import { AlertModule } from 'ngx-bootstrap';
 import { NetService } from "./services/net.service";
-
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'list', component: ListComponent  },
-  { path: 'detail/:id', loadChildren: "./pages/lazy-page/lazy-page.module#LazyPageModule"   },
-  { path: '**',   redirectTo: "" }
-];
 
 @NgModule({
   declarations: [
@@ -29,8 +21,6 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    // AlertModule.forRoot(),
-    RouterModule.forRoot(routes),
     HttpModule
   ],
   providers: [
